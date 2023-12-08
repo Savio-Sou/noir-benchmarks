ef449ae6de730b7741b243314ab91c90336e7e08
Savio <72797635+Savio-Sou@users.noreply.github.com>
2023-12-08 15:48:26 -0500

    Add `execute` benchmark script and numbers

--- scripts/benchmark_execute.sh
+++ scripts/benchmark_execute.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Specify the base commands
-base_command="nargo prove --package 2^"
-
 # Generate a sequence of arguments from 2 to 24
 arguments=($(seq 2 24))
 
 # Specify the output CSV file
-output_csv="results/prove.csv"
+output_csv="results/execute.csv"
 
 # Clear the contents of the output CSV file and write headers
-echo "Backend Circuit Size (2^n),\`nargo prove\` Time (s)" > "$output_csv"
+echo "Backend Circuit Size (2^n),\`nargo execute\` Time (s)" > "$output_csv"
 
 # Function to print a separator line
 print_separator() {
@@ -20,13 +17,13 @@ print_separator() {
 # Iterate through each set of arguments
 for args in "${arguments[@]}"; do
     # Construct the full commands
-    full_command="$base_command$args"
+    command="nargo execute witness_2^$args --package 2^$args"
 
-    # Print the prove command before timing
-    echo "Running: $full_command"
+    # Print the execute command before timing
+    echo "Running: $command"
 
-    # Run the prove command and capture the timing information
-    { time_output=$({ time $full_command; } 2>&1); }
+    # Run the execute command and capture the timing information
+    { time_output=$({ time $command; } 2>&1); }
 
     # Extract real time from time_output using awk
     real_time=$(echo "$time_output" | awk '/real/ {print $NF}')
